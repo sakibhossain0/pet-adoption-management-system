7e128139f386bcb220adc7a85a27f405e2732135
Rafia Anan <rafiaanan15@gmail.com>
2026-04-29 10:50:18 +0600

    Update QuizResponse.php

--- backend/app/Models/QuizResponse.php
+++ backend/app/Models/QuizResponse.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class QuizResponse extends Model
 {
-    //
+    protected $table = 'quiz_responses';
+    protected $primaryKey = 'rid';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'answer',
+        'uid',
+        'qid',
+    ];
 }
