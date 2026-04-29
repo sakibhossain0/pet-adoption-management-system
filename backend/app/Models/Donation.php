4a952859b09f0b0eaab2fa028110237af4f3db4f
tasminridita-sys <tasmin.ridita@gmail.com>
2026-04-29 19:31:06 +0600

    Update Donation.php

--- backend/app/Models/Donation.php
+++ backend/app/Models/Donation.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Donation extends Model
 {
-    //
+      protected $table = 'donations';
+    protected $primaryKey = 'did';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'status',
+        'date',
+        'amount',
+        'uid',
+        'shid',
+    ];
 }
