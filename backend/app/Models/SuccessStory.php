2975a0648ce5a61391828c34f9f1e0b041c16634
tasminridita-sys <tasmin.ridita@gmail.com>
2026-04-29 19:35:41 +0600

    Update SuccessStory.php

--- backend/app/Models/SuccessStory.php
+++ backend/app/Models/SuccessStory.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class SuccessStory extends Model
 {
-    //
+    protected $table = 'success_stories';
+    protected $primaryKey = 'story_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'title',
+        'story_text',
+        'photo_url',
+        'date',
+        'adoption_id',
+    ];
 }
