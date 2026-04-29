9ca363246f873e9be3962d84be52f30c5b294f12
Rafia Anan <rafiaanan15@gmail.com>
2026-04-29 10:56:33 +0600

    Update Adoption.php

--- backend/app/Models/Adoption.php
+++ backend/app/Models/Adoption.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Adoption extends Model
 {
-    //
+    protected $table = 'adoptions';
+    protected $primaryKey = 'adoption_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'adoption_date',
+        'app_id',
+    ];
 }
